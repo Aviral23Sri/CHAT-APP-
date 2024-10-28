d49f5b6d5be19d75e7a8c25b3dde9c45dc05d15a
Aviral Srivastava <kanha23srivastava@gmail.com>
2024-10-28 21:46:44 +0530

    use special class StreamBuilder in main.dart file in order to taaaaaaaaaaaaaaaaaaaexcare oser login state , fire sdk does all thing for us , a builder has a function which has two parameters a snapshot and a ctx , the snapshott tells about the user wheather rthe user is logged in or not..

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,15 @@
 import 'package:chat_app/screens/auth.dart';
+import 'package:chat_app/screens/chat.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-await Firebase.initializeApp(
-  options: DefaultFirebaseOptions.currentPlatform,
-);
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const App());
 }
 
@@ -25,7 +27,16 @@ class App extends StatelessWidget {
           seedColor: const Color.fromARGB(255, 63, 17, 177),
         ),
       ),
-      home: const AuthScreen(),
+      home: StreamBuilder(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (ctx, snapshot) {
+          if (snapshot.hasData) {
+            return const ChatScreen();
+          } else {
+            return const AuthScreen();
+          }
+        },
+      ),
     );
   }
 }
--- lib/screens/chat.dart
+++ lib/screens/chat.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class ChatScreen extends StatelessWidget {
-  ChatScreen({super.key});
+  const ChatScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
